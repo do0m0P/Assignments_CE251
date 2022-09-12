05d70af85b9fe22d35d35c3aeeb5d4845220771a
Aayush Jansari <90501879+do0m0P@users.noreply.github.com>
2022-09-12 13:08:53 +0530

    Add files via upload

---
+++ PART 4 Exception handling/src/Pr2_Throws/Throw.java
@@ -0,0 +1,31 @@
+package Pr2_Throws;
+
+import javax.lang.model.element.ModuleElement;
+
+class SampleException extends Exception{
+    SampleException(String s){
+        super(s);
+    }
+}
+public class Throw {
+    static double Divide(double a, double b) throws ArithmeticException{
+        throw new ArithmeticException("From func");
+        //return a/b;
+    }
+    public static void main(String[] args) {
+        try{
+            throw new SampleException("This is an exception......");
+        }catch(SampleException e){
+            System.out.println(e);
+        }
+        try{
+            double x = Divide(10,0);
+        }catch(ArithmeticException e){
+            System.out.println("Throws keyword used: ");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+}
